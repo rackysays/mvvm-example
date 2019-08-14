68b12d6a329eb8a10aa9582d9403370f6d035bc1
rickwolox <ricardo.marquez@wolox.com.ar>
2019-08-14 16:23:56 -0300

    Initial commit

---
+++ app/src/main/java/ar/com/wolox/android/mvvmexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package ar.com.wolox.android.mvvmexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
